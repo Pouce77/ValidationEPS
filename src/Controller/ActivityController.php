98810902d88d53a39c52ccb466685df30975c59b
Julien Kunze <julienkunze0@gmail.com>
2025-02-05 09:00:10 +0100

    📦 NEW: delete activity

--- src/Controller/ActivityController.php
+++ src/Controller/ActivityController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Activity;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -15,5 +16,15 @@ final class ActivityController extends AbstractController
         return $this->render('activity/index.html.twig', [
             'activity' => $activity,
         ]);
+    }
+
+    #[Route('/activity/delete/{id}', name: 'app_activity_delete')]
+    public function delete(Activity $activity, EntityManagerInterface $entityManager): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_PROF');
+        $entityManager->remove($activity);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_dashboard');
     }
 }
